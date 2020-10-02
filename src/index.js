40756d4650960ad198dfe9e704456208e2d1778d
WerJJJ <marikariwest@gmail.com>
2020-10-02 13:32:55 +0300

    feat: decided the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array != '' && array != undefined){
+  let min = array[0];
+  for (let i = 1; i < array.length; i++){
+    if ( array[i] < min ){
+      min = array[i];
+    }
+  }
+  return min;
+  }
+  return 0
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array != '' && array != undefined){
+  let max = array[0];
+  for (let i = 1; i < array.length; i++){
+    if (array[i] > max){
+      max = array[i];
+    }
+  }
+  return max;
+  }else {
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array != '' && array != undefined){
+    let sum = 0
+    let avg;
+    for (let i = 0; i < array.length; i++){
+      sum += array[i];
+    }
+    avg = sum / array.length;
+  
+    return avg;
+  }else{
+    return 0;
+  }
 }
